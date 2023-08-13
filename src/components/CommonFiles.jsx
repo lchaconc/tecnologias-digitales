e70805e5c33180f79cbc887fe88a885488b04a42
lchaconc <luis.chacon.campos@gmail.com>
2023-08-12 19:16:34 -0600

    se agrega efecto de despliegue -En  tarjetas en common files

--- src/components/CommonFiles.jsx
+++ src/components/CommonFiles.jsx
@@ -2,20 +2,43 @@ import { useState } from "react";
 import commonFiles from "../data/common-files.json";
 
 export default function CommonFiles() {
+  const [itemStates, setItemStates] = useState({});
+
+  const toggleCaption = (itemId) => {
+    setItemStates((prevStates) => ({
+      ...prevStates,
+      [itemId]: !prevStates[itemId],
+    }));
+  };
+
   return (
     <div className="row animate__animated animate__fadeInLeft">
       {commonFiles.map((item) => (
-        <div className="col-6"  key={item.id}>
+        <div className="col-6" key={item.id}>
           <div className="card mt-2">
-          <div className="card-header bg-danger">
-            <h4> {item.title} </h4>
-          </div>
-          <div className="card-body">
-            <p>{item.caption}</p>            
-          </div>
-          <div className="card-footer text-center">
-          <img className="img-m" src={`./files/${item.fileImg}`} alt="img" />
-          </div>
+            <div
+              role="button"
+              className="card-header bg-danger"
+              onClick={() => toggleCaption(item.id)}
+            >
+              <h4> {item.title} </h4>
+            </div>
+
+            {itemStates[item.id] && (
+              <>
+                <div className="card-body animate__animated animate__fadeInDown">
+                  <p>{item.caption}</p>
+                </div>
+
+                <div className="card-footer text-center animate__animated  animate__zoomIn">
+                  <img
+                    className="img-m"
+                    src={`./files/${item.fileImg}`}
+                    alt="img"
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
       ))}
