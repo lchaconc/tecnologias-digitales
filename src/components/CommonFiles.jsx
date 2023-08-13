aac3b8a01e0450013a3ac24e44eba09e7d1be773
lchaconc <luis.chacon.campos@gmail.com>
2023-08-12 19:03:26 -0600

    fininsumos para archivos comunes

--- src/components/CommonFiles.jsx
+++ src/components/CommonFiles.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import commonFiles from "../data/common-files.json";
 
 export default function CommonFiles() {
   return (
     <div className="row animate__animated animate__fadeInLeft">
       {commonFiles.map((item) => (
-        <div className="col-6 card mb-2"  key={item.id}>
-          <div className="card-header">
+        <div className="col-6"  key={item.id}>
+          <div className="card mt-2">
+          <div className="card-header bg-danger">
             <h4> {item.title} </h4>
           </div>
-          <div className="card-body text-center">
-            <p>{item.caption}</p>
-            <img className="img-m" src={`./files/${item.fileImg}`} alt="img" />
+          <div className="card-body">
+            <p>{item.caption}</p>            
+          </div>
+          <div className="card-footer text-center">
+          <img className="img-m" src={`./files/${item.fileImg}`} alt="img" />
+          </div>
           </div>
         </div>
       ))}
