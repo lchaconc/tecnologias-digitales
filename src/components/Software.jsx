0a470670ebc696122b9f16179e9dd799bf6ad604
lchaconc <luis.chacon.campos@gmail.com>
2023-08-29 07:54:43 -0600

    Software fase 2 -Montaje de carrusel -Despliegue de tarjetas -Renderizado de data software

--- src/components/Software.jsx
+++ src/components/Software.jsx
@@ -1,24 +1,54 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import software from "../data/software.json";
 
 export default function Software() {
+  const [itemStates, setItemStates] = useState({});
+
+  const toggleCaption = (itemId) => {
+    setItemStates((prevStates) => ({
+      ...prevStates,
+      [itemId]: !prevStates[itemId],
+    }));
+  };
+
   return (
     <div className="row animate__animated  animate__bounceInUp">
-      {software.map((item) => (
-        <Fragment>
-          <div className="col-6">
-            <div className="card mt-2">
+      <Carousel interval={null}>
+        {software.map((item) => (
+          <Carousel.Item key={item.id}>          
+                
+              <div className="card col-md-8 offset-md-2 mt-4 mb-5">
                 <div className="card-header">
-                <h3>{item.title} </h3>
-           
+                  <h3>{item.title} </h3>
                 </div>
                 <div className="card-body">
-                <p> {item.caption} </p>
+                  <p> {item.caption} </p>
+                <p className="text-center" >
+                <button
+                    className="btn btn-outline-info"
+                    onClick={() => toggleCaption(item.id)}
+                  >
+                    👁️
+                  </button>
+                </p>
                 </div>
-            </div>
-          </div>
-        </Fragment>
-      ))}
+                {itemStates[item.id] && (
+                  <div className="card-footer">
+                    <strong>Ejemplos:</strong>
+                    <ul>
+                      {item.examples.map((example, i) => (
+                        <li key={i}> {example} </li>
+                      ))}
+                    </ul>
+                    <strong>Consejos:</strong> {item.usageTips}
+                  </div>
+                )}
+              </div>
+            
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
